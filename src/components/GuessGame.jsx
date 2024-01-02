b50f2e49a76a3089a725baee83c87a88dfa1ff96
Milo2512 <milo.prelevicc@gmail.com>
2024-01-02 18:48:27 -0500

    Initial commit

--- src/components/GuessGame.jsx
+++ src/components/GuessGame.jsx
@@ -2,8 +2,118 @@ import { useState, useEffect } from "react";
 import ImageCard from "./ImageCard";
 import InputSection from "./InputSection";
 import GameOver from "./GameOver";
-import Images from "./Images";
 import "../index.css";
+
+const Images = [
+  {
+    name: "Allan Gardens",
+    description: "A conservatory and urban park located in the Garden District",
+    src: "Allan Gardens.jpeg",
+  },
+  {
+    name: "Bata Shoe Museum",
+    description:
+      "A museum displays over a thousand shoes and related artefacts",
+    src: "Bata Shoe Museum.jpeg",
+  },
+  {
+    name: "Berczy Park Fountain",
+    description: "A fountain with one cat and 27 cast iron dog statues ",
+    src: "Berczy Park Fountain.jpg",
+  },
+  {
+    name: "Chester Hill Lookout",
+    description: "Toronto's secret observation deck",
+    src: "Chester Hill Lookout.jpg",
+  },
+  {
+    name: "Crazy Doll House",
+    description: "Toronto's life-sized dollhouse",
+    src: "Crazy Doll House.jpg",
+  },
+  {
+    name: "Cube House",
+    description: "A one-of-a-kind home built with three tilted green boxes",
+    src: "Cube House.jpg",
+  },
+  {
+    name: "Gibraltar Point Lighthouse",
+    description: "The oldest existing lighthouse on the Great Lakes",
+    src: "Gibraltar Point Lighthouse.jpg",
+  },
+  {
+    name: "Gooderham Building",
+    description: "Once the head office of the Goodenham and Worts Distillery",
+    src: "Gooderham Building.jpg",
+  },
+  {
+    name: "Graffiti Alley",
+    description: "A massive swath of vibrant street art",
+    src: "Graffiti Alley.jpg",
+  },
+  {
+    name: "Guild Inn",
+    description: "Once a historic hotel in the Guildwood neighbourhood",
+    src: "Guild Inn.jpg",
+  },
+  {
+    name: "Half House",
+    description: "An old Victorian-era row house on St. Patrick Street",
+    src: "Half House.jpg",
+  },
+  {
+    name: "Ireland Park",
+    description:
+      "A park honours the Irish immigrants who fled during the famine of 1847",
+    src: "Ireland Park.jpeg",
+  },
+  {
+    name: "Kensington Market",
+    description: "A walkable bohemian neighbourhood",
+    src: "Kensington Market.jpg",
+  },
+  {
+    name: "Lower Bay Station",
+    description: "The Ghost Station or the TTC experiment gone wrong",
+    src: "Lower Bay Station.jpg",
+  },
+  {
+    name: "North Toronto Station",
+    description: "A former Canadian Pacific Railway station",
+    src: "North Toronto Station.jpg",
+  },
+  {
+    name: "Oculus Pavilion",
+    description: "A space-age park shelter",
+    src: "Oculus Pavilion.jpg",
+  },
+  {
+    name: "Old City Hall",
+    description: "A Romanesque-style civic building and court house",
+    src: "Old City Hall.jpg",
+  },
+  {
+    name: "Thomas Fisher Rare Book Library",
+    description:
+      "The largest repository of rare books and manuscripts in Canada",
+    src: "Thomas Fisher Rare Book Library.jpg",
+  },
+  {
+    name: "Redway Road Staircase",
+    description: "A set of wooden stairs located in Leaside",
+    src: "Redway Road Staircase.jpg",
+  },
+  {
+    name: "Ontario Place",
+    description: "An entertainment venue, event venue, and park in Toronto",
+    src: "Ontario Place.jpg",
+  },
+  {
+    name: "Winter Garden Theatre",
+    description: "The world's last operating double-decker theatre complex",
+    src: "Winter Garden Theatre.jpg",
+  },
+];
 
 const GuessGame = () => {
   const [currentImage, setCurrentImage] = useState({});
@@ -37,9 +147,8 @@ const GuessGame = () => {
 
     const randomIndex = Math.floor(Math.random() * remainingImages.length);
     const newImage = remainingImages[randomIndex];
-    console.log("New Image:", newImage);
-    setCurrentImage(newImage);
 
+    setCurrentImage(newImage);
     // Update the list of used images
     setUsedImages([...usedImages, newImage.src]);
   };
@@ -116,7 +225,7 @@ const GuessGame = () => {
   );
 
   const isGameOver = attempts >= 5;
-  console.log("Debug - currentImage.src:", currentImage.src);
+  
 
   return (
     <div className="backImg">
